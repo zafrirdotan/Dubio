038606b5e45dc931fc2200eff0995e9fd9b4848e
zafrir dotan <zafrir.dotan@gmail.com>
2020-10-06 22:08:33 +0300

    Updates

---
+++ precache-manifest.51b55e85ca9426094da42e71c3976464.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f363442938a9d79f037fe7732c2fd1cd",
+    "url": "/Dubio/index.html"
+  },
+  {
+    "revision": "792ed6897e97573ad759",
+    "url": "/Dubio/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "7bb7746573a45cd6bc66",
+    "url": "/Dubio/static/css/main.95aa3635.chunk.css"
+  },
+  {
+    "revision": "792ed6897e97573ad759",
+    "url": "/Dubio/static/js/2.f3c1a89a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Dubio/static/js/2.f3c1a89a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb7746573a45cd6bc66",
+    "url": "/Dubio/static/js/main.553eca29.chunk.js"
+  },
+  {
+    "revision": "b5f2152a94be277d191d",
+    "url": "/Dubio/static/js/runtime-main.bba5a71d.js"
+  },
+  {
+    "revision": "2aa2e633b1bb9feef423bc42e777a57c",
+    "url": "/Dubio/static/media/banner.2aa2e633.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dubio/precache-manifest.cb631c866accc9ba8ee0cead20e4b95c.js"
+  "/Dubio/precache-manifest.51b55e85ca9426094da42e71c3976464.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dubio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Dubio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

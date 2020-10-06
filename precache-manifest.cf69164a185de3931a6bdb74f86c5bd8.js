d06072a331edccf32ce23d27a5174f0fe5a9530c
zafrir dotan <zafrir.dotan@gmail.com>
2020-10-06 22:14:03 +0300

    Updates

---
+++ precache-manifest.cf69164a185de3931a6bdb74f86c5bd8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b457fe604e9e5b95d866212afb6a6b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "674410618f3065acc9ff",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "3dd0778c5a8a12b68b04",
+    "url": "/static/css/main.95aa3635.chunk.css"
+  },
+  {
+    "revision": "674410618f3065acc9ff",
+    "url": "/static/js/2.53de70b3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.53de70b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3dd0778c5a8a12b68b04",
+    "url": "/static/js/main.2e876ea1.chunk.js"
+  },
+  {
+    "revision": "ade55a2f0ce0e3610d56",
+    "url": "/static/js/runtime-main.2c5d6915.js"
+  },
+  {
+    "revision": "2aa2e633b1bb9feef423bc42e777a57c",
+    "url": "/static/media/banner.2aa2e633.png"
+  }
+]);

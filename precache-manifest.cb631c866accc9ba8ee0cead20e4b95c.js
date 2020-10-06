d2c342677530e0054cded2c6fae0f023e26471c9
zafrir dotan <zafrir.dotan@gmail.com>
2020-10-06 17:51:20 +0300

    Updates

---
+++ precache-manifest.cb631c866accc9ba8ee0cead20e4b95c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962a6a06f671373a65aba76be9404030",
+    "url": "/dubio/index.html"
+  },
+  {
+    "revision": "3db37382ba45cdfe137c",
+    "url": "/dubio/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "909ee9cabb08e054fcaf",
+    "url": "/dubio/static/css/main.95aa3635.chunk.css"
+  },
+  {
+    "revision": "3db37382ba45cdfe137c",
+    "url": "/dubio/static/js/2.97fd23de.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dubio/static/js/2.97fd23de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909ee9cabb08e054fcaf",
+    "url": "/dubio/static/js/main.d99c314c.chunk.js"
+  },
+  {
+    "revision": "9af40b231fa62e3a663c",
+    "url": "/dubio/static/js/runtime-main.9814a069.js"
+  },
+  {
+    "revision": "2aa2e633b1bb9feef423bc42e777a57c",
+    "url": "/dubio/static/media/banner.2aa2e633.png"
+  }
+]);

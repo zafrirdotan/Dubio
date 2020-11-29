260b5df71a367c9db5f2c9f845e8c787412fa813
moizes86 <moshe.mn86@gmail.com>
2020-11-29 23:58:54 +0200

    added articleReducer to rootReducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,18 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import userReducer from './Slices/UserSlice';
 import settingsReducer from './Slices/SettingsSlice';
-<<<<<<< HEAD
 import articleReducer from './Slices/ArticleSlice';
 
 const rootReducer = combineReducers({
   user: userReducer,
   settings: settingsReducer,
   articles: articleReducer
-=======
-const rootReducer = combineReducers({
-  user: userReducer,
-  settings: settingsReducer,
->>>>>>> 9b5ce8ad80a3e85ad8f3a9626a6bcf93ba5aba0b
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
